f7a8991ea66775b3abac2d15e4454653d1848e07
WJ951009 <fsdjin159@gmail.com>
2021-10-14 01:54:49 +0900

    Refactor: Redirectory

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-const path = require("path");
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "ejs");
-app.engine("html", require("ejs").renderFile);
+app.set('view engine', 'ejs');
+app.set('views', process.cwd() + "/src/views");
+app.engine('html', require('ejs').renderFile);
+app.use(express.static(__dirname + '/src'));
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/union', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
-db.on('error', (error) => console.log("DB 연결 실패", error));
+db.on('error', (error) => console.log('DB 연결 실패', error));
 db.once('open', () => {
     console.log('DB 연결');
 });
