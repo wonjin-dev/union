0155c3013874dd18034216e0426d23965e4dc7e2
WJ951009 <fsdjin159@gmail.com>
2021-10-20 03:08:52 +0900

    Refactor: Add name element of Character Schema

--- src/models/character.js
+++ src/models/character.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const characterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        required: true
+    },
     role: {
         type: String,
         required: true
